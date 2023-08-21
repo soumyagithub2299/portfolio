716b0c126c0ef3c2fb3b62197019c3c2589d7b68
Soumya Tiwari <soumyatiwari7247@gmail.com>
2023-08-21 17:42:32 +0530

    updated

--- script.js
+++ script.js
@@ -30,6 +30,10 @@ function efectoHabilidades(){
         habilidades[7].classList.add("creatividad");
         habilidades[8].classList.add("dedicacion");
         habilidades[9].classList.add("proyect");
+        habilidades[10].classList.add("azure");
+        habilidades[11].classList.add("leadership");
+
+
     }
 }
 
